00d344aa3b5bab30d046ea32160593e2ba2eb3b6
mvpnitesh <parimalanitesh@gmail.com>
2020-11-16 01:57:16 -0500

    Adding agent & direct interfaces to list of Supported Deploy Intrefaces
    Adding agent & direct interfaces to list of supported Deploy Interfaces
    
    Change-Id: Ib4522e967544087b4ef99de9c5929fcde431ebd0

--- sdflex_ironic_driver/sdflex_redfish/drivers.py
+++ sdflex_ironic_driver/sdflex_redfish/drivers.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from ironic.drivers.modules import agent
+from ironic.drivers.modules import iscsi_deploy
 from ironic.drivers import redfish
 
 from sdflex_ironic_driver.sdflex_redfish import bios as sdflex_bios
@@ -42,9 +44,11 @@ class SdflexRedfishHardware(redfish.RedfishHardware):
     @property
     def supported_bios_interfaces(self):
         """List of supported Bios interfaces."""
-        return [sdflex_bios.SdflexRedfishBios]
+        return [sdflex_bios.SdflexRedfishBios] + super(
+            SdflexRedfishHardware, self).supported_bios_interfaces
 
     @property
     def supported_deploy_interfaces(self):
         """List of supported Deploy interfaces."""
-        return [sdflex_deploy.SDFlexAgentDeploy]
+        return [sdflex_deploy.SDFlexAgentDeploy, agent.AgentDeploy,
+                iscsi_deploy.ISCSIDeploy]
